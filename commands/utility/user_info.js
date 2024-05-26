a8ba94f384ce5c46403ad66131908543b90766e7
pgnw <JoshuaWBragg@outlook.com>
2024-05-26 11:54:36 +1000

    Added user information command

---
+++ commands/utility/user_info.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require("discord.js")
+const { execute } = require("./Hello")
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('userInfo')
+    .setDescription("Displays information about the user."),
+
+    async execute(interaction)
+    {
+        await interaction.reply(`This command was run by ${interaction.user.username}, who has a user id of ${user.id}.`)
+    }
+    
+}

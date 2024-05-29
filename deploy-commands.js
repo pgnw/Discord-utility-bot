59979a02834ec1396516e98816a00c8847617c46
pgnw <JoshuaWBragg@outlook.com>
2024-05-30 07:27:08 +1000

    Command deployer added

---
+++ deploy-commands.js
@@ -0,0 +1,45 @@
+const { REST, Routes } = require('discord.js');
+const { clientId, guildId, token } = require('./config.json');
+const filesystem = require('node:fs');
+const path = require('node:path');
+
+const commands = [];
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = filesystem.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = filesystem.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+            commands.push(command.data.toJSON());
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing the data or execute property.`);
+        }
+    }
+}
+
+const rest = new REST().setToken(token);
+
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} slash commands.`);
+
+        const data = await rest.put(
+            Routes.applicationGuildCommands(clientId, guildId),
+            { body: commands },
+        )
+
+        console.log(`Successfully reloaded ${data.length} slash commands.`);
+    }
+    catch (e) {
+        console.error(e);
+    }
+
+
+
+})();

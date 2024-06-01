cdb8b3be00f8bfa003c2be380661f1cb2e6ded96
pgnw <JoshuaWBragg@outlook.com>
2024-06-01 12:22:24 +1000

    User info command fixed

--- commands/utility/user_info.js
+++ commands/utility/user_info.js
@@ -3,12 +3,11 @@ const { execute } = require("./Hello")
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName('user_info')
-    .setDescription("Displays information about the user."),
+        .setName('user_info')
+        .setDescription("Displays information about the user."),
 
-    async execute(interaction)
-    {
-        await interaction.reply(`This command was run by ${interaction.user.username}, who has a user id of ${user.id}.`)
+    async execute(interaction) {
+        await interaction.reply(`This command was run by ${interaction.user.username}, who has a user id of ${interaction.user.id}.`)
     }
-    
+
 }

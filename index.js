27d86946b043b2ce6c47648e2599f9b9fae81abd
pgnw <JoshuaWBragg@outlook.com>
2024-06-09 12:49:32 +1000

    Command deployment fixed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, Gate] });
 
 client.commands = new Collection();
 

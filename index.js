bb688c2be119c65520d2367c6572e2f449e8f5d7
pgnw <JoshuaWBragg@outlook.com>
2024-06-13 12:17:19 +1000

    Fixed intents error

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds, Gate] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
 
